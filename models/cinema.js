10b94f22f708fb57efea50d7a3c4a70fdf411526
Analia Hojman <analia.hojman@ubiome.com>
2017-08-21 00:39:43 -0300

    Refactor models to use dao

--- models/cinema.js
+++ models/cinema.js
@@ -1,33 +1,25 @@
 'use strict';
 
-//
-// http://docs.aws.amazon.com/amazondynamodb/latest/gettingstartedguide/GettingStarted.NodeJs.04.html#GettingStarted.NodeJs.04.Scan
-//
-// AWS Dynamo SCAN
+var _ = require('underscore');
+var dao = require('../daos/dao');
 
-var cinemas = [];
-for (var index = 0; index <= 20; index++) {
-  var cinema = {
-    id: index,
-    name: 'Cine ' + index
-  };
-  cinemas.push(cinema)
-}
+var cinemaTable = 'TOWN_Cinema';
+var key = 'movie';
+
+// var cinemas = [];
+// for (var index = 0; index <= 20; index++) {
+//   var cinema = {
+//     id: index,
+//     name: 'Cine ' + index
+//   };
+//   cinemas.push(cinema)
+// }
 
 exports.list = function(callback) {
   console.log('All Cinemas found.');
-  callback(null, cinemas);
+  return dao.findAll(cinemaTable, ["movie", "category", "date", "duration"], callback);
 };
 
-// AWS Dynamo Query (findOne)
 exports.findOne = function(cinemaId, callback) {
-
-  var cinema = cinemas[cinemaId];  
-  if (!cinema) {
-    console.log('Cinema not found.');
-    return callback('Cinema not found.', null);
-  }
-  
-  console.log('Cinema ' + cinemaId + ' found.');
-  return callback(null, cinema);
+  return dao.findById(cinemaTable, key, cinemaId, callback);
 };
--- models/store.js
+++ models/store.js
@@ -1,33 +1,44 @@
 'use strict';
 
-//
-// http://docs.aws.amazon.com/amazondynamodb/latest/gettingstartedguide/GettingStarted.NodeJs.04.html#GettingStarted.NodeJs.04.Scan
-//
-// AWS Dynamo SCAN
+var dao = require('../daos/dao');
+var _ = require('underscore');
 
-var stores = [];
-for (var index = 0; index <= 20; index++) {
-  var store = {
-    id: index,
-    name: 'Tienda ' + index
-  };
-  stores.push(store)
-}
+var storeTable = 'TOWN_Stores';
+var key = 'name';
 
 exports.list = function(callback) {
-  console.log('All Stores found.');
-  callback(null, stores);
+  console.log('All stores found.');
+  return dao.findAll(storeTable, ["name"], callback);
 };
 
-// AWS Dynamo Query (findOne)
-exports.findOne = function(storeId, callback) {
-
-  var store = stores[storeId];  
-  if (!store) {
-    console.log('Store not found.');
-    return callback('Store not found.', null);
-  }
-  
-  console.log('Store ' + storeId + ' found.');
-  return callback(null, store);
+exports.findOne = function(cinemaId, callback) {
+  return dao.findById(storeTable, key, cinemaId, callback);
 };
+
+
+// var stores = [];
+// for (var index = 0; index <= 20; index++) {
+//   var store = {
+//     id: index,
+//     name: 'Tienda ' + index
+//   };
+//   stores.push(store)
+// }
+//
+// exports.list = function(callback) {
+//   console.log('All Stores found.');
+//   callback(null, stores);
+// };
+//
+// // AWS Dynamo Query (findOne)
+// exports.findOne = function(storeId, callback) {
+//
+//   var store = stores[storeId];
+//   if (!store) {
+//     console.log('Store not found.');
+//     return callback('Store not found.', null);
+//   }
+//
+//   console.log('Store ' + storeId + ' found.');
+//   return callback(null, store);
+// };
