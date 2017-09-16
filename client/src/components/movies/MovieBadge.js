fa8eb2ba0508c30b998fe1bb31179ab634335f5f
Pablo Angiorama <pabloangiorama@gmail.com>
2017-09-16 12:31:10 -0300

    Movies slider fix

--- client/src/components/movies/MovieBadge.js
+++ client/src/components/movies/MovieBadge.js
@@ -5,17 +5,18 @@ class MovieBadge extends React.Component {
 	const s3url = 'https://s3.amazonaws.com/towncenterweb/peliculas/'
     const padding1 = { padding: 1 }
     const margin0 = { margin:0 }
+    const minHeight = { minHeight:40 }
     return (
-		<div className='badge resto-badge'>
+		<div className='badge movie-badge'>
 			<div className='row' style={margin0}>
-				<img className='img-responsive col-xs-12 resto-badge-img' alt=''
+				<img className='img-responsive col-xs-12 movie-badge-img' alt=''
 					style={padding1}
 					src={s3url + this.props.movie.web_image_url} />
 	    	</div>
 			<div className='row' style={margin0}>
-				<div className='col-xs-12 store-sm-txt'>
-					<p className='center padding-v'><strong>{this.props.movie.movie}</strong></p>
-					<p className='center'>{this.props.movie.hours}</p>
+				<div className='col-xs-12 movie-sm-txt'>
+					<p className='center padding-v' style={minHeight}><strong>{this.props.movie.movie}</strong></p>
+					<p className='center' style={minHeight}>{this.props.movie.hours}</p>
 					<br />
 		    	</div>
 	    	</div>
