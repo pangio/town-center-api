e50e056d4bd5fdf4552a5eff3bf07cbf92af5037
Pablo Angiorama <pabloangiorama@gmail.com>
2017-09-18 19:23:46 -0300

    Section buttons mobile

--- client/src/components/layout/Sections.js
+++ client/src/components/layout/Sections.js
@@ -10,25 +10,50 @@ class Sections extends React.Component {
     return (
         <div className="sections-container">
             <h2 className='background'><span>Experiencia Towncenter</span></h2>
-            <div className="sections-boxes">
+            <div className="sections-boxes desktop">
                 <div className="row center">
                     <NavLink to="/tiendas" >
                       <ImgCache
-                        classNames={'img-responsive col-xs-3 sections-img'}
+                        classNames={'sections-img'}
                         src={s3url + 'btn_compras.png'} />
                     </NavLink>
                     <NavLink to="/" >
                       <ImgCache
-                        classNames={'img-responsive col-xs-3 sections-img'}
+                        classNames={'sections-img'}
                         src={s3url + 'btn_eventos.png'} />
                     </NavLink>
                     <NavLink to="/restaurantes" >
-                      <ImgCache classNames={'img-responsive col-xs-3 sections-img'}
+                      <ImgCache classNames={'sections-img'}
                         src={s3url + 'btn_quieroiracomer.png'} />
                     </NavLink>
                     <NavLink to="/cines" >
                       <ImgCache
-                        classNames={'img-responsive col-xs-3 sections-img'}
+                        classNames={'sections-img'}
+                        src={s3url + 'btn_vamosacine.png'} />
+                    </NavLink>
+                </div>
+            </div>
+            <div className="sections-boxes mobile">
+                <div className="row center">
+                    <NavLink to="/tiendas" >
+                      <ImgCache
+                        classNames={'sections-img'}
+                        src={s3url + 'btn_compras.png'} />
+                    </NavLink>
+                    <NavLink to="/" >
+                      <ImgCache
+                        classNames={'sections-img'}
+                        src={s3url + 'btn_eventos.png'} />
+                    </NavLink>
+                </div>
+                <div className="row center">
+                    <NavLink to="/restaurantes" >
+                      <ImgCache classNames={'sections-img'}
+                        src={s3url + 'btn_quieroiracomer.png'} />
+                    </NavLink>
+                    <NavLink to="/cines" >
+                      <ImgCache
+                        classNames={'sections-img'}
                         src={s3url + 'btn_vamosacine.png'} />
                     </NavLink>
                 </div>
