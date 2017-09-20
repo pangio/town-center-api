3f22bad35f083ca8476df29dc73e01c562ba39bd
Pablo Angiorama <pabloangiorama@gmail.com>
2017-09-20 00:27:49 -0300

    Ofertas: empty list error msg

--- client/src/components/sales/SalesList.js
+++ client/src/components/sales/SalesList.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import SaleBadge from './SaleBadge'
 
+function SalesListEmpty(props) {
+  return (
+    <div className='row center padding-50 notification'>
+      <p>No hay ofertas disponibles.</p>
+      <p>Vuelve pronto!</p>
+    </div>
+  )
+}
+
 class SalesList extends React.Component {
   render() {
     return (
       <div>
         {
-          this.props.salesList.map((sale, i) => {
+          this.props.salesList.length == 0 &&
+          <SalesListEmpty/>
+        }
+        {
+          this.props.salesList.length > 0 && this.props.salesList.map((sale, i) => {
             return (
               <SaleBadge sale={sale} key={i} />
             )
