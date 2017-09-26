4da4cf0f5e172f02b718203c0f66c9f939da2884
Pablo Angiorama <pabloangiorama@gmail.com>
2017-09-26 15:07:32 -0300

    Header refactor: Resto

--- client/src/components/restaurants/Restaurantes.js
+++ client/src/components/restaurants/Restaurantes.js
@@ -41,8 +41,22 @@ class Restaurantes extends Component {
       <div className='center page-container'>
         <h2 className='background restaurantes'><span>¡Quiero ir a comer!</span></h2>
 
-        <img className="img-responsive" alt=''
-            src='https://s3.amazonaws.com/towncenterweb/assets/header-resto.png' />
+        <div className='row page-container header-container'>
+          <div className='col-md-6'>
+            <div className='row'>
+              <img className='img-responsive header-donde-img' alt=''
+                src='https://s3.amazonaws.com/towncenterweb/assets/img_donde.png' />
+            </div>
+            <div className='row'>
+              <h2 className='restaurantes green-header'><span>¿Vamos a comer?</span></h2>
+            </div>
+          </div>
+
+          <div className='col-md-6 header-main-img'>
+            <img className='img-responsive' alt=''
+                src='https://s3.amazonaws.com/towncenterweb/assets/headerphone-resto.png' />
+          </div>
+        </div>
 
         {
           this.state.isLoading &&
