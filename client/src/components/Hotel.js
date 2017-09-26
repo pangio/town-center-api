15d86ce5cf1221ff002928a1b587c5b1b3b665e2
Pablo Angiorama <pabloangiorama@gmail.com>
2017-09-26 14:40:42 -0300

    Header refactor: Hotel

--- client/src/components/Hotel.js
+++ client/src/components/Hotel.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Hotel extends Component {
   render() {
     return (
 		<div className='center page-container'>
             <h2 className='background hotel'><span>¿Hotel?</span></h2>
-	        <img className="img-responsive" alt=''
-	            src='https://s3.amazonaws.com/towncenterweb/assets/header-hotel.png' />
+
+			<div className='row page-container header-container'>
+				<div className='col-md-6'>
+					<div className='row'>
+						<img className='img-responsive header-donde-img' alt=''
+							src='https://s3.amazonaws.com/towncenterweb/assets/img_donde.png' />
+					</div>
+					<div className='row'>
+						<h2 className='hotel green-header'>
+							<span>Alojamiento</span>
+						</h2>
+					</div>
+				</div>
+				<div className='col-md-6 header-main-img'>
+			        <img className="img-responsive" alt=''
+			            src='https://s3.amazonaws.com/towncenterweb/assets/headerphone-hotel.png' />
+				</div>
+			</div>
 
 	        { this.props.children }
 
